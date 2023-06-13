e386550c82991029181735ab6de438383a926ab0
dromn1 <nhamode250@gmail.com>
2023-06-13 19:18:30 +0300

    update

--- components/Content.js
+++ components/Content.js
@@ -61,14 +61,14 @@ import Link from 'next/link';
             
         </div>
 
-        <div className="col-xxl-5 model-height d-flex justify-content-center animate__animated animate__fadeInUp animate__delay-0s position-relative">
-          {/* <Canvas>
+        <div className="col-xxl-5 model-height d-flex justify-content-center position-relative mysvg">
+          <Canvas className="mysvg">
           <OrbitControls enableZoom={false} enableDamping={false} enablePan={false} autoRotate/>
             <Suspense fallback={null}>
               <Model />
               <Environment preset="sunset"  />
             </Suspense>
-          </Canvas> */}
+          </Canvas>
           {/* <Model_3d/> */}
           
           
